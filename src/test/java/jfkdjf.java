7d3d45c30c4f7a3de5d8ade02205abb42334cf8a
toan1231 <100801887+toan1231@users.noreply.github.com>
2022-10-08 22:44:41 -0400

    Update jfkdjf.java

--- src/test/java/jfkdjf.java
+++ src/test/java/jfkdjf.java
@@ -1,15 +1,5 @@
        driver= Driver.getInstance().getDriver();
-        driver.get("https://www.amazon.com/");
-        try {
-            Wait wait=new WebDriverWait(driver,20);
-            if(wait.until(ExpectedConditions.alertIsPresent())==null){
-                System.out.println("none happening");
-            }else {
-                Alert alert=driver.switchTo().alert();
-                alert.accept();
-            }
-        }catch (NoAlertPresentException en){
-            en.printStackTrace();
+     
         }
 
 
