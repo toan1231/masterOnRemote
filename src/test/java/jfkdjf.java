7fe1a9fa1907b8a7e388a73648bd6f4b1575da5e
toan1231 <100801887+toan1231@users.noreply.github.com>
2022-10-08 22:30:46 -0400

    Update jfkdjf.java

--- src/test/java/jfkdjf.java
+++ src/test/java/jfkdjf.java
@@ -1,6 +1,6 @@
 public class jfkdjf {
     public static void main(String[] args) {
         System.out.println("this is brand new project gonna push");
-        //00000000000000000000000000000000000000000
+        //000000000
     }
 }
